33595c090c4111ecac02fcbe806c75a18b805a1f
Connor Petersen <cmpete@gmail.com>
2018-04-27 00:33:34 -0700

    adding mapping examples and images

---
+++ maps/test_hex_250.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="test_hex_250" tilewidth="250" tileheight="250" tilecount="2" columns="0">
+ <tileoffset x="-17" y="0"/>
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="250" height="250" source="blue.png"/>
+ </tile>
+ <tile id="2">
+  <image width="250" height="250" source="green.png"/>
+ </tile>
+</tileset>
