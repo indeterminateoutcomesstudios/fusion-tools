b989ee25c8a1101ca11f807e97afb739d3b33eaf
Connor Petersen <cmpete@gmail.com>
2018-04-27 18:18:34 -0700

    more map stuff

--- maps/test_hex_250.tsx
+++ maps/test_hex_250.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset name="test_hex_250" tilewidth="250" tileheight="250" tilecount="2" columns="0">
+<tileset name="test_hex_250" tilewidth="250" tileheight="250" tilecount="3" columns="0">
  <tileoffset x="-17" y="0"/>
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
@@ -7,5 +7,8 @@
  </tile>
  <tile id="2">
   <image width="250" height="250" source="green.png"/>
+ </tile>
+ <tile id="3">
+  <image width="250" height="250" source="empty.png"/>
  </tile>
 </tileset>
