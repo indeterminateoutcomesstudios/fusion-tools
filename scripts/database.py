8d0f09e45c94459d061dd41468c6eb7115b69fab
Connor Petersen <cmpete@gmail.com>
2018-05-08 15:46:43 -0700

    updating with covered tests for most database functions

--- scripts/database.py
+++ scripts/database.py
@@ -10,6 +10,7 @@
 #experience tracking to PCs
 
 import sqlite3
+from datetime import datetime, date, time
 import pprint
 
 Request = 'R'
@@ -22,13 +23,14 @@ Denied = 'D'
 
 class Backend():
     '''Represents the database which contains game history and player status'''
-    def __init__(self, name='game.db'):
+    def __init__(self, name='game.db', debug=False):
         '''Create connection to sqlite database file on disk and initialize cursor to perform actions'''
-        self.connection = sqlite3.connect(name, check_same_thread=False)
+        self.connection = sqlite3.connect(name, detect_types=sqlite3.PARSE_DECLTYPES, check_same_thread=False)
         self.cursor = self.connection.cursor()
+        self.debug = debug
         
-    def execute(self, command=''):
-        self.cursor.execute(command)
+    def execute(self, command='', values=()):
+        self.cursor.execute(command, values)
 
     def executescript(self, script=''):
         if script:
@@ -40,9 +42,11 @@ class Backend():
     def fetchall(self):
         return self.cursor.fetchall()
     
-    def get_result(self):
+    def get_1_col_result(self):
         rs = self.fetchall()
-        if len(rs) == 1:
+        if len(rs) == 0:
+            return []
+        elif len(rs) == 1:
             return rs[0][0]
         else:
             return [r[0] for r in rs]
@@ -63,7 +67,7 @@ class Backend():
     def destroy_db(self):
         '''Drop tables to effectively clear/reset database'''
         for table in self.get_tables():
-            self.execute("""drop table {}""".format(table))
+            self.execute("drop table {}".format(table))
         self.commit()
 
     # def backup_db(self):
@@ -78,12 +82,12 @@ class Backend():
     
     def format_table(self, table_name=''):
         '''return the contents of the specified table as a formatted string'''
-        self.execute("""select * from {}""".format(table_name))
+        self.execute("select * from {}".format(table_name))
         return pprint.pformat(self.fetchall())
 
     def get_tables(self):
-        self.execute("""select name from sqlite_master where type = 'table'""")
-        return self.get_result()
+        self.execute("select name from sqlite_master where type = ?", ('table',))
+        return [t[0] for t in self.fetchall()]
         
     def check_db(self):
         '''Function to sanity check that tables exist in the database'''
@@ -94,47 +98,118 @@ class Backend():
             return False
 
     def add_player(self, fullname, email='', phone=0):
-        self.execute("""insert into player (fullname, email, phone) values ('{}', '{}', {})""".format(fullname, email, phone))
+        self.execute("insert into player (fullname, email, phone) values (?, ?, ?)", (fullname, email, phone))
         self.commit()
 
     def get_player_id(self, fullname=None, email=None, phone=None, reddit=None, discord=None):
         if fullname:
-            self.execute("""select id from player where fullname == '{}'""".format(fullname))
+            self.execute("select id from player where fullname == ?", (fullname,))
         elif email:
-            self.execute("""select id from player where email == '{}'""".format(email))
+            self.execute("select id from player where email == ?", (email,))
         elif phone:
-            self.execute("""select id from player where fullname == '{}'""".format(fullname))
+            self.execute("select id from player where fullname == ?", (fullname,))
         elif reddit:
-            self.execute("""select id from player inner join reddit where player.id = reddit.player_id and reddit.account == '{}'""".format(reddit))
+            self.execute("select id from player inner join reddit where player.id = reddit.player_id and reddit.account == ?", (reddit,))
         elif discord:
-            self.execute("""select id from player inner join discord where player.id = discord.player_id and discord.account == '{}'""".format(discord))
-        return self.get_result()
+            self.execute("select id from player inner join discord where player.id = discord.player_id and discord.account == ?", (discord,))
+        return self.get_1_col_result()
 
     def associate_social_with_player(self, player_id, account, act_type):
         if act_type == 'reddit':
-            self.execute("""insert into reddit (player_id, account) values ('{}', '{}')""".format(player_id, account))
+            self.execute("insert into reddit (player_id, account) values (?, ?)", (player_id, account))
         elif act_type == 'discord':
-            self.execute("""insert into discord (player_id, account) values ('{}', '{}')""".format(player_id, account))
+            self.execute("insert into discord (player_id, account) values (?, ?)", (player_id, account))
         else:
-            print('unrecognized account type: {}'.format(act_type))
+            if self.debug:
+                print('\nunrecognized account type: {}'.format(act_type))
 
     def get_social_of_player(self, player_id, act_type):
         if act_type == 'discord':
-            self.execute("""select account from discord where player_id == {}""".format(player_id))
+            self.execute("select account from discord where player_id == {}".format(player_id))
         elif act_type == 'reddit':
-            self.execute("""select account from reddit where player_id == {}""".format(player_id))
+            self.execute("select account from reddit where player_id == {}".format(player_id))
         else:
-            print('unrecognized account type: {}'.format(act_type))
-        return self.get_result()
+            if self.debug:
+                print('\nunrecognized account type: {}'.format(act_type))
+        return self.get_1_col_result()
 
-    def add_game(self, status=Request):
-        pass
+    def add_game(self, date=None, time=None, location='', vtt=False, status=Request):
+        dt = datetime.combine(date, time)
+        self.execute("insert into game (status, datetime, location, vtt) values (?, ?, ?, ?)", (status, dt, location, vtt))
+        self.commit()
 
-    def update_game_status(self, game_id, status):
-        pass
+    def get_game_id(self, date=None, vtt=None):
+        if date and vtt:
+            self.execute("select id from game where datetime like '%{}%' and vtt = ?".format(date), (vtt,))
+        elif date:
+            self.execute("select id from game where datetime like '%{}%'".format(date))
+        # elif vtt: # doesn't seem useful
+        #     self.execute("select id from game where vtt = ?", (vtt,))
+        return self.get_1_col_result()
+
+    def get_all_games(self):
+        self.execute("select * from game")
+        return self.fetchall()
+
+    def update_game(self, game_id, date=None, time=None, location=None, vtt=None, status=None):
+        if date or time:
+            if date and time:
+                dt = datetime.combine(date, time)
+            else:
+                self.execute("select datetime from game where id = ?", (game_id,))
+                old_dt = self.fetchall()[0][0]
+                old_date = old_dt.date()
+                old_time = old_dt.time()
+                if date:
+                    dt = datetime.combine(date, old_time)
+                else:
+                    dt = datetime.combine(old_date, time)
+            self.execute("update game set (datetime) = (?) where id = ?", (dt, game_id))
+        if location:
+            self.execute("update game set (location) = (?) where id = ?", (location, game_id))
+        if vtt != None:
+            self.execute("update game set (vtt) = (?) where id = ?", (vtt, game_id))
+        if status:
+            self.execute("update game set (status) = (?) where id = ?", (status, game_id))
+        self.commit()
+
+    def add_dm_to_game(self, player_id, game_id):
+        self.execute("insert into game_dms (game_id, dm_id) values (?, ?)", (game_id, player_id))
+        self.commit()
+
+    def add_character_to_game(self, character_id, game_id):
+        self.execute("insert into game_pcs (game_id, character_id) values (?, ?)", (game_id, character_id))
+        self.commit()
+
+    def get_attendees(self, game_id):
+        dm_ids = self.get_dms(game_id)
+        if not isinstance(dm_ids, list):
+            dm_ids = [dm_ids]
+        player_ids = self.get_players_of_game(game_id)
+        if not isinstance(player_ids, list):
+            player_ids = [player_ids]
+        return [dm_ids, player_ids]
+
+    def get_dms(self, game_id):
+        self.execute("select id from player inner join game_dms where player.id = game_dms.dm_id and game_dms.game_id == ?", (game_id,))
+        return self.get_1_col_result()
+
+    def get_players_of_game(self, game_id):
+        self.execute("select player_id from character inner join game_pcs where character.id = game_pcs.character_id and game_pcs.game_id == ?", (game_id,))
+        return self.get_1_col_result()
 
     def add_character_to_player(self, player_id, name):
-        pass
+        self.execute("insert into character (player_id, name) values (?, ?)", (player_id, name))
+        self.commit()
+
+    def get_character_id(self, player_id, name):
+        self.execute("select id from character where player_id = ? and name = ?", (player_id, name))
+        return self.get_1_col_result()
+
+    def get_characters(self, player_id):
+        self.execute("select id, name from character where player_id == ?", (player_id,))
+        pcs = self.fetchall()
+        return pcs
 
     def award_tokens(self, num_tokens=1, tkn_type='player'):
         if tkn_type == 'player':
@@ -142,19 +217,39 @@ class Backend():
         elif tkn_type == 'author':
             pass
         else:
-            print('unrecognized token type: {}'.formt(tkn_type))
+            if self.debug:
+                print('\nunrecognized token type: {}'.formt(tkn_type))
 
     def transfer_tokens(self, giv_id, rec_id, num_tokens):
         if giv_id == rec_id:
             return
 
-    def add_wealth(self, character_id, gold=0.0, date=None):
-        pass
+    def add_wealth(self, character_id, gold=0.0, game_id=None):
+        self.execute("insert into wealth (character_id, game_id, gold_value) values (?, ?, ?)", (character_id, game_id, gold))
+        self.commit()
 
-    def add_experience(self, character_id, exp=0, date=None):
-        pass
+    def get_character_wealth(self, character_id):
+        self.execute("select * from wealth where character_id == ?", (character_id,))
+        txs = self.fetchall()
+        total = 0.0
+        for tx in sorted(txs, key=lambda x: x[1]): #sorting by game number...perhaps by date would be better
+            total += tx[2]
+        return total
 
-    def add_inventory(self, character_id, name='', date=None):
-        pass
+    def add_experience(self, character_id, exp=0, game_id=None):
+        self.execute("insert into experience (character_id, game_id, amount) values (?, ?, ?)", (character_id, game_id, exp))
+        self.commit()
+
+    def get_character_experience(self, character_id):
+        self.execute("select * from experience where character_id == ?", (character_id,))
+        txs = self.fetchall()
+        total = 0
+        for tx in sorted(txs, key=lambda x: x[1]): #sorting by game number...perhaps by date would be better
+            total += tx[2]
+        return total
+
+    def add_inventory(self, character_id, name='', game_id=None):
+        self.execute("insert into inventory (character_id, game_id, name) values (?, ?, ?)", (character_id, game_id, name))
+        self.commit()
 
 
--- scripts/test_database.py
+++ scripts/test_database.py
@@ -1,10 +1,11 @@
 import unittest
-from database import Backend
+from datetime import datetime, date, time
+from database import *
 
 class BackendTest(unittest.TestCase): 
   
   def setUp(self):
-    self.db = Backend('test.db')
+    self.db = Backend('test.db', debug=True)
     self.db.destroy_db()
     self.db.initialize_db()
     
@@ -22,6 +23,7 @@ class BackendTest(unittest.TestCase):
     self.assertFalse(self.db.check_db())
 
   def test_player(self):
+    # TODO: enforce uniqueness?
     self.db.add_player('John Doe','johndoe@example.com',5555555555)
     self.db.execute('select * from player where id == 1')
     f = self.db.fetchone()
@@ -52,10 +54,95 @@ class BackendTest(unittest.TestCase):
     self.assertEqual(new_pid, pid)
 
   def test_game(self):
-    pass
+    # TODO: multiple games on the same day?
+    d = date(2018,6,25)
+    t = time(20,30,0)
+    location = "1845 35th Ave, Hillsboro, OR 97116"
+    vtt = False
+    status = Request
+    self.db.add_game(d, t, location, vtt, status)
+    # self.db.add_game(d, t, location, vtt, status)
+    games = self.db.get_all_games()
+    self.assertEqual(games[0], (1, Request, datetime(2018, 6, 25, 20, 30), '1845 35th Ave, Hillsboro, OR 97116', 0))
+    # self.assertEqual(games[1], (2, 'R', '2018-06-25 20:30:00', '1845 35th Ave, Hillsboro, OR 97116', 0))
+    gid = self.db.get_game_id(d)
+    self.assertEqual(gid, 1)
+
+    self.db.update_game(gid, location='Underdark')
+    self.db.update_game(gid, vtt=True)
+    self.db.update_game(gid, status=Denied)
+    self.db.update_game(gid, date=date(2018,6,26))
+    self.db.update_game(gid, time=time(20,0,0))
+    games = self.db.get_all_games()
+    self.assertEqual(games[0], (1, Denied, datetime(2018, 6, 26, 20), 'Underdark', 1))
+    self.db.update_game(gid, date=date(2018,7,7), time=time(22, 34, 0), location='Earth', vtt=False, status=Accepted)
+    games = self.db.get_all_games()
+    self.assertEqual(games[0], (1, Accepted, datetime(2018, 7, 7, 22, 34), 'Earth', 0))
+
+    # dm connection
+    self.db.add_player('Evil DM','edm@example.com',6666666666)
+    self.db.add_player('Player A','pa@example.com',1111111111)
+    self.db.add_player('Player B','pb@example.com',2222222222)
+    self.db.add_player('Player C','pc@example.com',3333333333)
+    pid = self.db.get_player_id(fullname='Evil DM')
+    self.db.add_dm_to_game(pid, gid)
+
+    # pc connection    
+    pid = self.db.get_player_id(fullname='Player A')
+    self.db.add_character_to_player(pid, 'Francis')
+    cid = self.db.get_character_id(pid, 'Francis')
+    self.db.add_character_to_game(cid, gid)
+
+    pid = self.db.get_player_id(fullname='Player B')
+    self.db.add_character_to_player(pid, 'Franklin')
+    cid = self.db.get_character_id(pid, 'Franklin')
+    self.db.add_character_to_game(cid, gid)
+
+    pid = self.db.get_player_id(fullname='Player C')
+    self.db.add_character_to_player(pid, 'Ferdinand')
+    cid = self.db.get_character_id(pid, 'Ferdinand')
+    self.db.add_character_to_game(cid, gid)
+
+    # player + dm list from pcs
+    attendees = self.db.get_attendees(gid)
+    self.assertListEqual(attendees, [[1],[2,3,4]])
+
+    self.db.add_game(d, t, location, True, status)
+    gid = self.db.get_game_id(date=d, vtt=True)
+    pid = self.db.get_player_id(fullname='Evil DM')
+    self.db.add_dm_to_game(pid, gid)
+    pid = self.db.get_player_id(fullname='Player A')
+    cid = self.db.get_character_id(pid, 'Francis')
+    self.db.add_character_to_game(cid, gid)
+    attendees = self.db.get_attendees(gid)
+    self.assertListEqual(attendees, [[1],[2]])
+
+    # post connection
+    # write-up connections
+    # map connections
+    # game status
 
   def test_character(self):
-    pass
+    self.db.add_player('Player A','pa@example.com',1111111111)
+    pid = self.db.get_player_id(fullname='Player A')
+    self.assertEqual(pid, 1)
+    self.db.add_character_to_player(pid, 'Francis')
+    pcs = self.db.get_characters(player_id=pid)
+    self.assertEqual(pcs, [(1,'Francis')])
+    # experience
+    self.db.add_experience(pcs[0][0], exp=4000, game_id=1)
+    self.db.add_experience(pcs[0][0], exp=4000, game_id=2)
+    self.db.add_experience(pcs[0][0], exp=4000, game_id=3)
+    exp = self.db.get_character_experience(pcs[0][0])
+    self.assertEqual(exp, 12000)
+    # wealth
+    # TODO: online transaction date for when talking with merchant
+    self.db.add_wealth(pcs[0][0], gold=100.25, game_id=1)
+    self.db.add_wealth(pcs[0][0], gold=1000.25, game_id=1)
+    self.db.add_wealth(pcs[0][0], gold=-200.0, game_id=2)
+    cash = self.db.get_character_wealth(pcs[0][0])
+    self.assertEqual(cash, 900.5)
+    # inventory
 
 if __name__ == '__main__': # pragma: no cover
   unittest.main()
